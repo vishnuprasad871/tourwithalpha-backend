8f3c14214c450778461ca394be036129e0b88d24
joffy96 <vishnuprasad871@gmain.com>
2026-03-07 18:43:07 +0530

    Added Grid for this

--- app/code/Tourwithalpha/BookingCount/Model/OfflineSalesDataProvider.php
+++ app/code/Tourwithalpha/BookingCount/Model/OfflineSalesDataProvider.php
@@ -8,11 +8,16 @@ declare(strict_types=1);
 
 namespace Tourwithalpha\BookingCount\Model;
 
+use Magento\Framework\Api\Filter;
 use Magento\Ui\DataProvider\AbstractDataProvider;
 use Tourwithalpha\BookingCount\Model\ResourceModel\OfflineSales\CollectionFactory;
 
 /**
- * Data provider for the offline sales UI component grid and form
+ * Data provider for the offline sales UI component grid and form.
+ *
+ * AbstractDataProvider is the correct base for *both* grids and forms in Magento 2.
+ * For grids the framework calls addFilter / addOrder / setLimit on the provider before
+ * calling getData(), so we must delegate those calls to the underlying collection.
  */
 class OfflineSalesDataProvider extends AbstractDataProvider
 {
@@ -22,12 +27,12 @@ class OfflineSalesDataProvider extends AbstractDataProvider
     private array $loadedData = [];
 
     /**
-     * @param string $name
-     * @param string $primaryFieldName
-     * @param string $requestFieldName
+     * @param string            $name
+     * @param string            $primaryFieldName
+     * @param string            $requestFieldName
      * @param CollectionFactory $collectionFactory
-     * @param array $meta
-     * @param array $data
+     * @param array             $meta
+     * @param array             $data
      */
     public function __construct(
         string $name,
@@ -42,9 +47,56 @@ class OfflineSalesDataProvider extends AbstractDataProvider
     }
 
     /**
-     * Retrieve data for grid
+     * Add field filter to collection.
      *
-     * @return array
+     * Called by the grid framework for each active filter.
+     *
+     * @param Filter $filter
+     * @return void
+     */
+    public function addFilter(Filter $filter): void
+    {
+        $field = $filter->getField();
+        $condType = $filter->getConditionType() ?: 'eq';
+        $value = $filter->getValue();
+
+        // Date-range filters arrive as ['from' => ..., 'to' => ...] arrays
+        if (is_array($value)) {
+            $this->collection->addFieldToFilter($field, $value);
+        } else {
+            $this->collection->addFieldToFilter($field, [$condType => $value]);
+        }
+    }
+
+    /**
+     * Add sorting to collection.
+     *
+     * @param string $field
+     * @param string $direction
+     * @return void
+     */
+    public function addOrder(string $field, string $direction): void
+    {
+        $this->collection->addOrder($field, strtoupper($direction));
+    }
+
+    /**
+     * Set pagination on collection.
+     *
+     * @param int $offset
+     * @param int $size
+     * @return void
+     */
+    public function setLimit(int $offset, int $size): void
+    {
+        $this->collection->setPageSize($size);
+        $this->collection->setCurPage(ceil($offset / $size) + 1);
+    }
+
+    /**
+     * Retrieve data for the grid in the expected format.
+     *
+     * @return array{items: array, totalRecords: int}
      */
     public function getData(): array
     {
@@ -59,7 +111,7 @@ class OfflineSalesDataProvider extends AbstractDataProvider
 
         $this->loadedData = [
             'items' => $items,
-            'totalRecords' => $this->collection->getSize(),
+            'totalRecords' => (int) $this->collection->getSize(),
         ];
 
         return $this->loadedData;
